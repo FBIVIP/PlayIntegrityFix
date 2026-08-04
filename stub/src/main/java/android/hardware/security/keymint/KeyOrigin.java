efde063979339739bb73a2438c1f804a8fb64e20
PlayIntegrityFix: Phantom Edition by FATEH <159710616+FBIVIP@users.noreply.github.com>
2026-08-04 12:49:09 +0000

    TrickyStore OSS v3 fateh7 — XOR misc path

--- stub/src/main/java/android/hardware/security/keymint/KeyOrigin.java
+++
@@ -1,9 +0,0 @@
-package android.hardware.security.keymint;
-
-public @interface KeyOrigin {
-    int GENERATED = 0;
-    int DERIVED = 1;
-    int IMPORTED = 2;
-    int RESERVED = 3;
-    int SECURELY_IMPORTED = 4;
-}
--- stub/src/main/java/android/os/SELinux.java
+++
@@ -1,8 +0,0 @@
-package android.os;
-
-public class SELinux {
-    public static boolean checkSELinuxAccess(
-            String scon, String tcon, String tclass, String perm) {
-        throw new UnsupportedOperationException("STUB!");
-    }
-}
